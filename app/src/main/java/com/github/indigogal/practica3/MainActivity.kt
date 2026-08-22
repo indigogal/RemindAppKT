aa90c62a64571daccee57c0baff580f9fe87910a
azurova <111108245+indigogal@users.noreply.github.com>
2026-08-21 18:32:50 -0700

    Test Card

--- app/src/main/java/com/github/indigogal/practica3/MainActivity.kt
+++ app/src/main/java/com/github/indigogal/practica3/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTheme() {
                 Box(
-                    modifier = Modifier.fillMaxSize(),
+          modifier = Modifier.fillMaxSize(),
                     contentAlignment = Alignment.Center
                 ){
                     ReminderCard()
